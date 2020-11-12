b0a557c924814d3b8cc1ce704af8a662f057cb13
Ivan <ivanbezdomniygit@gmail.com>
2020-11-12 11:24:03 +0300

    initial

---
+++ src/main/java/ru/homeless/CoronaDesinfector.java
@@ -0,0 +1,12 @@
+package ru.homeless;
+
+public class CoronaDesinfector {
+    public void start(Room room) {
+        desinfect(room);
+    }
+
+    private void desinfect(Room room) {
+        System.out.println("Зачитывается молитва: 'корона изыди!' - молитва прочитана, вирус низвургнут в ад");
+    }
+
+}
---
+++ src/main/java/ru/homeless/Room.java
@@ -0,0 +1,5 @@
+package ru.homeless;
+
+public class Room {
+
+}
